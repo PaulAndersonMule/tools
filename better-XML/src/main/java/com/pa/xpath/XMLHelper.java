d26d389bd2070c22d8361d87341e412230a6d51b
Kun Li <kun.li@mulesoft.com>
2019-04-27 01:19:40 -0400

    restore XMLHelper.java from upstream; removed wrong pom.xml file.

--- better-XML/src/main/java/com/pa/xpath/XMLHelper.java
+++ better-XML/src/main/java/com/pa/xpath/XMLHelper.java
@@ -2,8 +2,6 @@ package com.pa.xpath;
 
 import com.pa.xpathutils.NamespaceResolverImpl;
 import com.pa.xpathutils.XPathUtils;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import net.sf.saxon.s9api.SaxonApiException;
 import net.sf.saxon.s9api.XdmItem;
@@ -14,18 +12,13 @@ import net.sf.saxon.s9api.XdmItem;
  */
 public abstract class XMLHelper {
 
-  private static List<XdmItem> internalGetXPathResult(NamespaceResolverImpl nsrImpl, String xpath, String fileContents) throws SaxonApiException {
-      return XPathUtils.evaluate(xpath, fileContents, nsrImpl);
-  }
-
   protected static String getXPathResults(NamespaceResolverImpl nsrImpl, String xpath, String fileContents) {
-    List<XdmItem> xdmItems = null;
+    List<XdmItem> xdmItems;
     try {
-        xdmItems = internalGetXPathResult(nsrImpl, xpath, fileContents);
-    } catch (SaxonApiException e) {
-        return e.getMessage();
+      xdmItems = XPathUtils.evaluate(xpath, fileContents, nsrImpl);
+    } catch (SaxonApiException ex) {
+      return ex.getMessage();
     }
-
     StringBuilder sb = new StringBuilder();
 		xdmItems.stream().forEach((item) -> {
 			sb.append(item.toString()).append('\n');
@@ -33,20 +26,4 @@ public abstract class XMLHelper {
     
     return sb.toString();
   }
-
-  public static List<String> getXPathResultList(NamespaceResolverImpl nsrImpl, String xpath, String fileContents) {
-        List<XdmItem> xdmItems = null;
-        List<String> result = new ArrayList<>();
-        try {
-            xdmItems = internalGetXPathResult(nsrImpl, xpath, fileContents);
-        } catch (SaxonApiException e) {
-            result.add(e.getMessage());
-            return result;
-        }
-        for (XdmItem item : xdmItems) {
-            result.add(item.toString());
-        }
-        return result;
-    }
-
 }
